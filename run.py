f228811a8eb87edbb6b3f319e90d79bd1110079d
azimny86 <azimny86@gmail.com>
2023-01-12 23:00:24 +0000

    Add basic components for game

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
 import random
+
+"""
+    Cards for players
+"""
+cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+
+"""
+    Player and computer cards
+"""
+compHand = []
+playerHand = []
