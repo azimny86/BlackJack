bcd1536de1b20983b7fd631ae0f5ee50109c987f
azimny86 <azimny86@gmail.com>
2023-01-14 17:42:29 +0000

    Update determinations of the winner and the loser with more score difference comments and adding Draw

--- run.py
+++ run.py
@@ -93,9 +93,11 @@ if scoring(playerHand) == 21:
 elif scoring(compHand) == 21:
     print("Black Jack !! Dealer Wins :(")
     print(f"Unfortunately you lose your opponent has {compHand}  and you only {playerHand}")
+    print(f"You lost, you had {scoring(playerHand)} points and your opponent have {scoring(compHand)} points")
 elif scoring(playerHand) > 21:
     print("You give up ! Your opponent win :( ")
     print(f"Unfortunately you lose your opponent has {compHand}  and you only {playerHand}")
+    print(f"You lost, you had {scoring(playerHand)} points and your opponent have {scoring(compHand)} points")
 elif scoring(compHand) > 21:
     print(" You win !!! :)")
     print(f"You win {playerHand}  your opponent has {compHand}")
@@ -103,8 +105,11 @@ elif scoring(compHand) > 21:
 elif 21 - scoring(compHand) < 21 - scoring(playerHand):
     print("Dealer Wins!")
     print(f"Unfortunately you lose your opponent has {compHand} points and you only {playerHand}")
+    print(f"You lost, you had {scoring(playerHand)} points and your opponent have {scoring(compHand)} points")
 elif 21 - scoring(compHand) > 21 - scoring(playerHand):
     print("You win")
     print(f"You win, you had {playerHand}  and your opponent only {compHand}")
+    
     print(f"You win, you had {scoring(playerHand)} points and your opponent only {scoring(compHand)}")
-
+elif scoring(playerHand) == scoring(compHand):
+    print("Draw")
